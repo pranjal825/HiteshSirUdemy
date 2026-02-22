81a5e2eee1783b27d448fe09e96b44be7c5f61fc
Pranjal Dwivedi <pranjaldwivedi9628@gmail.com>
2026-02-22 22:51:20 +0530

    Learning

---
+++ Basic Js/part 1/changes.js
@@ -0,0 +1,11 @@
+let gameName = "GTA IV"
+
+gameName = "GTA V"
+
+console.log(gameName);
+
+const username ="Prandwiv";
+
+username = "Pranjal";
+
+console.log(username)
---
+++ Basic Js/part 1/printing.js
@@ -0,0 +1,6 @@
+console.log("hello");
+process.stdout.write("Pranjal");
+console.table("test");
+
+console.table({State: "Uttar Pradesh", city: "Kanpur"}); 
+console.warn("Pranjal"); 
