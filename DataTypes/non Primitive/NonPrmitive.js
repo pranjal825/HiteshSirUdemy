7c2282bab8220b765e482d3f4b18643d7018a89c
Pranjal Dwivedi <pranjaldwivedi9628@gmail.com>
2026-03-22 22:39:11 +0530

    Changes

--- DataTypes/non Primitive/NonPrmitive.js
+++ DataTypes/non Primitive/NonPrmitive.js
@@ -1,4 +1,56 @@
-let  = {};
+/*let username = {
+    firstname: "Pranjal",
+isloggedin: true,
+};
+console.log (username);
+console.log(typeof username);*/
 
-console.log ();
-console.log(typeof );
+/*const username = {
+    firstname: "Pranjal",
+isloggedin: true,
+};
+
+username.firstname = "Aman";
+username.lastname = "Dwivefi";
+
+console.log (username.firstname);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);x
+
+
+const username = {
+    "first name": "Pranjal",
+isloggedin: true,
+};
+
+username.firstname = "Aman";
+username.lastname = "Dwivefi";
+
+console.log (username["firstname"]);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);const username = {
+    firstname: "Pranjal",
+isloggedin: true,
+};
+
+username.firstname = "Aman";
+username.lastname = "Dwivefi";*/
+
+/*console.log (username.firstname);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);*/
+
+/*let today = new Date();
+console.log(today.getDate());*/
+
+//let anotherUser = ["Aman", true]
+
+//console.log(anotherUser[0]);
+
+//Implicit Type Conversion
+let isvalue = "2abc";
+console.log(typeof(Number(isvalue)));
+console.log(Number(null))
---
+++ DataTypes/non Primitive/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+console.log(username.lastname);
+console.log(username);
