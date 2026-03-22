aadfcab18666904baf61c2a1cda2bb4313e682cf
Pranjal Dwivedi <pranjaldwivedi9628@gmail.com>
2026-03-22 23:34:45 +0530

    Conditons

---
+++ DataTypes/part3/condition challenges.js
@@ -0,0 +1,86 @@
+// checking a number is greater than another number:
+
+/*let num1 = 5;
+let num2 = 8;
+console.log("I am regular upper code");
+
+if(num1 > num2) {
+    console.log("num 1 is greater than num2");
+}
+
+console.log("i am regular bottom code");*/
+
+/*let num1 = 5;
+let num2 = 8;
+console.log("I am regular upper code");
+
+if(num1 < num2) {
+    console.log("num 1 is greater than num2");
+}
+
+console.log("i am regular bottom code");*/
+
+/*let num1 = 5;
+let num2 = 8;
+console.log("I am regular upper code");
+
+if(num1 > num2) {
+    console.log("num 1 is greater than num2");
+} else {
+console.log("Nope, num1 is Not Greater")
+}
+
+console.log("i am regular bottom code");*/
+
+// checking if a string is equal to another string:
+
+/*let username = "pranjal";
+let anotherUsername ="pranjal";
+
+if (username == anotherUsername) {
+    console.log("Pick another Username");
+} else {
+    console.log("You can pick this username");
+}*/
+
+// Checking a variable is number or not;
+
+let score = 44
+
+/*if (typeof score === 'number') {
+    console.log("Yep,this is a number");
+} else {
+    console.log("No that is not a number");
+}*/
+
+// checking if a boolean value is true or false:
+
+let isTeaReady = false;
+
+if(isTeaReady) {
+    console.log("Tea is Ready");
+}else {
+    //console.log("Tea is Not Ready");
+}
+
+// checking if an array is empty or not:
+
+/*let items = [];
+
+console.log(items.length);
+
+if(items.length === 0) {
+console.log("Array is Empty");
+} else {
+    console.log("Array is not Empty");
+}*/
+
+let items = ["item"];
+
+console.log(items.length);
+
+if(items.length === 0) {
+console.log("Array is Empty");
+} else {
+    console.log("Array is not Empty");
+}
