949eed734d9e7b7bcf1e1c4c5b8e9aaeb62ae440
Pranjal Dwivedi <pranjaldwivedi9628@gmail.com>
2026-03-24 00:02:32 +0530

    Qstns

---
+++ DataTypes/part3/array challenges.js
@@ -0,0 +1,55 @@
+/* 
+1 Declare an array named 'teaflavours' that contains the strings "green tea", "black tea", and "oolomg tea".
+
+Access the first element of the array and store it in a variable named 'firsttea'.*/
+
+let teaflavours =["green tea", "black tea", "oolong tea"]
+
+const firstTea = teaflavours[2];
+
+//console.log(firstTea);
+
+/* 2 
+Declare an array named 'cities' containing "London","Tokyo", "Paris", and "New York". 
+
+Acces the third element in the array and store it in a variable named 'favoriteCity'.*/
+
+let cities =["London", "Tokyo","Paris","New York"];
+
+ const favoriteCity = cities[3];
+
+ //console.log(favoriteCity);
+
+ /* you have an array named 'teaTypes containing "herbal tea","white tea", and "masala chai".
+
+ Change the second element of the array to "jasmine tea"*/
+ let teaTypes = ["herbal tea","white tea", "masala chai"]
+// change second element
+teaTypes[1] = "jasmine tea";
+
+//console.log(teaTypes);
+
+/* declare an array named "CitiesVisited" containing "Gurgaon" and "Mohali".
+
+Add "Banglore" to the array using the 'push' method. */
+
+//let citiesVisited = ["Gurgaon","mohali"];
+//citiesVisited[1] = "Banglore";
+//console.log(citiesVisited);
+
+//let citiesVisited = ["Gurgaon","mohali"];
+//citiesVisited[citiesVisited.length - 1] = "Banglore";
+//console.log(citiesVisited);
+// Another way
+//console.log(citiesVisited.length);
+// another way
+
+/*let citiesVisited = ["Gurgaon","mohali"];
+citiesVisited.push("Banglore");
+console.log(citiesVisited);*/
+/*
+let citiesVisited = ["Mumbai","Sydney"];
+citiesVisited.push("Bangalore");
+
+console.log(citiesVisited);*/
+
